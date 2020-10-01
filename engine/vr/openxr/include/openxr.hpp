ffae6a3c84783b7c5ab8a9d319ed602622e048f7
redstrate <54911369+redstrate@users.noreply.github.com>
2020-09-30 21:27:03 -0400

    Create XR instance

--- engine/vr/openxr/include/openxr.hpp
+++ engine/vr/openxr/include/openxr.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+
 #include "vr.hpp"
 
 class OpenXR : public VR {
 public:
 	void initialize() override;
+
+	XrInstance instance = XR_NULL_HANDLE;
 };
--- engine/vr/openxr/src/openxr.cpp
+++ engine/vr/openxr/src/openxr.cpp
@@ -1,12 +1,23 @@
 #include "openxr.hpp"
 
-#include <openxr/openxr.h>
-#include <openxr/openxr_platform.h>
-
 #include "log.hpp"
+#include "assertions.hpp"
+
+#define XR_CHECK(x) Expects(x == XR_SUCCESS)
 
 void OpenXR::initialize() {
-	console::debug(System::Core, "OpenXR is initialized!");
-
+	XrApplicationInfo application_info = {};
 	
+	strcpy(application_info.applicationName, "Engine Test");
+	strcpy(application_info.engineName, "Prism");
+
+	application_info.apiVersion = XR_VERSION_1_0;
+
+	XrInstanceCreateInfo create_info = {};
+	create_info.type = XR_TYPE_INSTANCE_CREATE_INFO;
+	create_info.applicationInfo = application_info;
+
+	XR_CHECK(xrCreateInstance(&create_info, &instance));
+
+	console::debug(System::Core, "OpenXR is initialized!");
 }
