9312e510ea7287a19105b3cf30d4c44418fb4a00
redstrate <54911369+redstrate@users.noreply.github.com>
2021-02-18 14:35:05 -0500

    Update openxr loader

--- engine/vr/openxr/src/openxr.cpp
+++ engine/vr/openxr/src/openxr.cpp
@@ -8,16 +8,17 @@
 void OpenXR::initialize() {
 	XrApplicationInfo application_info = {};
 	
-	strcpy(application_info.applicationName, "Engine Test");
-	strcpy(application_info.engineName, "Prism");
-
-	application_info.apiVersion = XR_VERSION_1_0;
+	strncpy(application_info.applicationName, "Engine Test", XR_MAX_APPLICATION_NAME_SIZE);
+	strncpy(application_info.engineName, "Prism", XR_MAX_ENGINE_NAME_SIZE);
+	application_info.applicationVersion = 1;
+	application_info.engineVersion = 0;
+	application_info.apiVersion = XR_CURRENT_API_VERSION;
 
 	XrInstanceCreateInfo create_info = {};
 	create_info.type = XR_TYPE_INSTANCE_CREATE_INFO;
 	create_info.applicationInfo = application_info;
 
-	XR_CHECK(xrCreateInstance(&create_info, &instance));
+	console::debug(System::Core, "{}", std::to_string(xrCreateInstance(&create_info, &instance)));
 
 	console::debug(System::Core, "OpenXR is initialized!");
 }
